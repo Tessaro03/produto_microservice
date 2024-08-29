0e2f30adaf2c44bf86751129b0e65b259c08f2aa
Gabriel Tessaro Favareto <gabrieltessarofavareto@gmail.com>
2024-08-29 13:29:50 -0300

    Adicionado AllArgsConstructor

--- src/main/java/com/produto/model/Produto.java
+++ src/main/java/com/produto/model/Produto.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Produto {
     
